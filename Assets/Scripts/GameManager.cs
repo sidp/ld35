525ccc6037ccdc6ec6f89b49330c5b9a71976ae8
Peter Simonsson <sidp86@gmail.com>
2016-04-17 16:27:19 +0200

    Added a Game Manager that keeps track of the score

---
+++ Assets/Scripts/GameManager.cs
@@ -0,0 +1,47 @@
+﻿using UnityEngine;
+using UnityEngine.UI;
+using System.Collections;
+
+public class GameManager : MonoBehaviour {
+
+	[SerializeField]
+	private GameObject playerObject;
+	
+	[SerializeField]
+	private GameObject scoreTextObject;
+	private Text scoreText;
+	
+	[SerializeField]
+	protected int score = 0;
+	private int pickupScore = 0;
+	private int distanceScore = 0;
+	private float playerStartedFromX;
+	private float playerFurtherestX;
+
+	// Use this for initialization
+	void Start () {
+		playerStartedFromX = playerObject.transform.position.x;
+		scoreText = scoreTextObject.GetComponent<Text>();
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		UpdateScore(); 
+	}
+	
+	void UpdateScore () {
+		if (playerObject.transform.position.x > playerFurtherestX) {
+			playerFurtherestX = playerObject.transform.position.x;
+		}
+
+		float distanceScoreBasis = playerFurtherestX - playerStartedFromX;
+		distanceScore = (int) distanceScoreBasis;
+		score = distanceScore + pickupScore;
+
+		scoreText.text = "SCORE: " + score + " pts";
+	}
+
+	void AddScore (int score) {
+		pickupScore += score;
+	}
+}
